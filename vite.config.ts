da8088680ce4568b9d0c3d38fc2d107b9f723e50
Rok Kambic <rok.kambic@telekom.si>
2026-09-09 18:35:17 +0200

    svelte

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sveltekit({
+			files: {
+				assets: 'frontend/static',
+				src: 'frontend/src'
+			},
+			outDir: 'frontend/.svelte-kit',
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
